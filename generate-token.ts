0646dc2df0c8603d06e950b6be91bdb4fb220912
abbieives35 <164657247+abbieives35@users.noreply.github.com>
2025-07-16 21:34:24 -0400

    Update generate-token.ts

--- generate-token.ts
+++ generate-token.ts
@@ -1,4 +1,9 @@
-const jwt = require("jsonwebtoken");
+// generate-token.ts
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
 
-const token = jwt.sign({ id: "1" }, "default_secret");
+dotenv.config();
+
+const token = jwt.sign({ id: "1" }, process.env.JWT_SECRET || "default_secret");
 console.log("JWT:", token);
+
